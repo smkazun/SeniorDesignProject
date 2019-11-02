571dbd55e7e2ab228a2a6e85947cc783fcacad77
pjd <pjd@iastate.edu>
2019-11-02 10:32:22 -0500

    Begin implementing ability to view users and delete them

---
+++ OpenSourceBlog/OpenSourceBlog/Controllers/AdminController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OpenSourceBlog.Models;
+using Microsoft.AspNet.Identity;
+
+namespace OpenSourceBlog.Controllers
+{
+    public class AdminController : Controller
+    {
+
+        private ApplicationDbContext _context;
+
+        public AdminController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        // GET: Admin
+        public ActionResult ManageUsers()
+        {
+            return View();
+        }
+    }
+}
