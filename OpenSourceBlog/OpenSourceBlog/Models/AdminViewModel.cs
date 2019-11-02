859b04a6066816e03bc4fe851de0ba1981cafff8
pjd <pjd@iastate.edu>
2019-11-02 10:32:22 -0500

    Begin implementing ability to view users and delete them

---
+++ OpenSourceBlog/OpenSourceBlog/Models/AdminViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OpenSourceBlog.Models
+{
+    public class AdminViewModel
+    {
+    }
+
+    public class ManageUsersViewModel
+    {
+        public int Id { get; }
+
+        public string Email { get; }
+
+        public string UserName { get; }
+
+        public string Role { get; }
+    }
+}
