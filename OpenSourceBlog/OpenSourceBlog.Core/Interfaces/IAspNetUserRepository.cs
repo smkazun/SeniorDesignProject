2568efab7be7c60b965640b2a6738d026fa71759
pjd <pjd@iastate.edu>
2019-11-02 13:15:49 -0500

    Commit

---
+++ OpenSourceBlog/OpenSourceBlog.Core/Interfaces/IAspNetUserRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenSourceBlog.OpenSourceBlog.Core.Models;
+
+namespace OpenSourceBlog.Core.Interfaces
+{
+    public interface IAspNetUserRepository
+    {
+        IEnumerable<AspNetUser> SelectAll();
+        AspNetUser SelectByUserName(string username);
+        void Insert(AspNetUser user);
+        void Update(AspNetUser user);
+        void Delete(string username);
+        void Save();
+    }
+}
