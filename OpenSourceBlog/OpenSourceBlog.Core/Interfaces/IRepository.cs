1ebe52f792bca9dcd9298ae05b25bf3f16c5b465
pjd <pjd@iastate.edu>
2019-11-02 23:59:42 -0500

    Set repository interface to use generic types instead.

---
+++ OpenSourceBlog/OpenSourceBlog.Core/Interfaces/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenSourceBlog.Core.Interfaces
+{
+    public interface IRepository<T, U> where T :class
+    {
+        IEnumerable<T> GetAll();
+        T Get(U id);
+        void Create(T entity);
+        void Update(U id, T entityNew);
+        void Delete(U id);
+    }
+}
