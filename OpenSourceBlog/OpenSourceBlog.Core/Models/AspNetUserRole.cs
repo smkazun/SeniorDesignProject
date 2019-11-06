7a89626a74de12484b98ac4c4a0cbcf24363775e
pjd <pjd@iastate.edu>
2019-11-05 23:47:03 -0600

    Before update of ALL packages across ALL projects

--- OpenSourceBlog/OpenSourceBlog.Core/Models/AspNetUserRole.cs
+++ OpenSourceBlog/OpenSourceBlog.Core/Models/AspNetUserRole.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,12 @@ namespace OpenSourceBlog.Core.Models
 {
     public class AspNetUserRole
     {
+        [Key]
+        [Required]
+        [MaxLength(128)]
         public string UserId { get; set; }
+        [Required]
+        [MaxLength(128)]
         public string RoleId { get; set; }
-        public virtual AspNetRole AspNetRole { get; set; }
     }
 }
