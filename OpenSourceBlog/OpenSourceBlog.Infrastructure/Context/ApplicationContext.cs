04a5d623238a2d54994d48663e5dfffa6b42cff2
pjd <pjd@iastate.edu>
2019-11-05 18:45:56 -0600

    Merge context files into a single one

---
+++ OpenSourceBlog/OpenSourceBlog.Infrastructure/Context/ApplicationContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenSourceBlog.Core.Models;
+
+namespace OpenSourceBlog.Infrastructure.Context
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext() : base("name=DefaultConnection")
+        {
+
+        }
+
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Page> Pages { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<PostCategory> PostCategories { get; set; }
+        public DbSet<PostComment> PostComments { get; set; }
+        public DbSet<PostNotify> PostNotifies { get; set; }
+        public DbSet<PostTag> PostTags { get; set; }
+        public DbSet<Profile> Profiles { get; set; }
+        public DbSet<Right> Rights { get; set; }
+        public DbSet<RightRole> RightRoles { get; set; }
+        public DbSet<Setting> Settings { get; set; }
+        public DbSet<StopWord> StopWords { get; set; }
+        //public DbSet<User> Users { get; set; }
+        //public DbSet<UserRole> UserRoles { get; set; }
+        //public DbSet<Role> Roles { get; set; }
+    }
+}
