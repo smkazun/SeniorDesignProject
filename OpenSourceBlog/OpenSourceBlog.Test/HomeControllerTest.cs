1cdd8a51fbe20b25231986ca753cfa9128d5364a
Sebastian Kazun <smkazun@iastate.edu>
2020-02-16 23:57:23 -0600

    Finished testing sorting

--- OpenSourceBlog/OpenSourceBlog.Test/HomeControllerTest.cs
+++ OpenSourceBlog/OpenSourceBlog.Test/HomeControllerTest.cs
@@ -7,6 +7,8 @@ using OpenSourceBlog.Database.Repositories;
 using System.Web.Mvc;
 using OpenSourceBlog.Database.Models;
 using OpenSourceBlog.Database.Interfaces;
+using Moq;
+using System.Linq;
 
 namespace OpenSourceBlog.Test
 {
@@ -16,80 +18,15 @@ namespace OpenSourceBlog.Test
     [TestClass]
     public class HomeControllerTest
     {
-        private IPostRepository repo;
 
         public HomeControllerTest()
-        {
-            //
-            // TODO: Add constructor logic here
-            //
-            repo = new PostRepository();
-
-            //make an example post
-            Post p = new Post();
-            
-            p.BlogId = Guid.Parse("71acc52b-040c-4456-8820-dd21f6122fbc");
-            p.PostId = Guid.NewGuid();
-            p.Title = "MostRecent";
-            p.Description = "Description";
-            p.PostContent = "<p>Sample blog post</p>";
-            p.DateCreated = new DateTime(2019, 3, 27);
-            p.DateModified = DateTime.Now;
-            p.Author = "SortingTest";
-            p.IsPublished = false;
-            p.IsCommentEnabled = false;
-            p.Rating = 5;
-            repo.Create(p);
-
-            Post p1 = new Post();
-
-            p1.BlogId = Guid.Parse("87acc52b-040c-4456-8820-dd21f6122fbc");
-            p1.PostId = Guid.NewGuid();
-            p1.Title = "LeastRecent";
-            p1.Description = "Description";
-            p1.PostContent = "<p>Sample blog post</p>";
-            p1.DateCreated = new DateTime(2015, 5, 10);
-            p1.DateModified = DateTime.Now;
-            p1.Author = "SortingTest";
-            p1.IsPublished = false;
-            p1.IsCommentEnabled = false;
-            p1.Rating = 2;
-            repo.Create(p1);
-
-            Post p2 = new Post();
-
-            p2.BlogId = Guid.Parse("11acc52b-040c-4456-8820-dd21f6122fbc");
-            p2.PostId = Guid.NewGuid();
-            p2.Title = "Highest Rating";
-            p2.Description = "Description";
-            p2.PostContent = "<p>Sample blog post</p>";
-            p2.DateCreated = new DateTime(2018, 6, 13);
-            p2.DateModified = DateTime.Now;
-            p2.Author = "SortingTest";
-            p2.IsPublished = false;
-            p2.IsCommentEnabled = false;
-            p2.Rating = 10;
-            repo.Create(p2);
+        {  
 
         }
 
         private TestContext testContextInstance;
 
-        /// <summary>
-        ///Gets or sets the test context which provides
-        ///information about and functionality for the current test run.
-        ///</summary>
-        public TestContext TestContext
-        {
-            get
-            {
-                return testContextInstance;
-            }
-            set
-            {
-                testContextInstance = value;
-            }
-        }
+        
 
         #region Additional test attributes
         //
@@ -114,44 +51,128 @@ namespace OpenSourceBlog.Test
         #endregion
 
         
-        [TestMethod]
-        public void TestIndexView()
+
+
+
+        private List<Post> GetPosts()
         {
+            var postList = new List<Post>();
+            postList.Add(new Post()
+            {
+                BlogId = Guid.Parse("71acc52b-040c-4456-8820-dd21f6122fbc"),
+                PostId = Guid.NewGuid(),
+                Title = "Most Recent",
+                Description = "Description",
+                PostContent = "<p>Sample blog post</p>",
+                DateCreated = new DateTime(2019, 3, 27),
+                DateModified = DateTime.Now,
+                Author = "SortingTest",
+                IsPublished = true,
+                IsCommentEnabled = false,
+                Rating = 5
+        });
+            postList.Add(new Post()
+            {
+                BlogId = Guid.Parse("81acc52b-040c-4456-8820-dd21f6122fbc"),
+                PostId = Guid.NewGuid(),
+                Title = "Least Recent",
+                Description = "Description",
+                PostContent = "<p>Sample blog post</p>",
+                DateCreated = new DateTime(2015, 5, 10),
+                DateModified = DateTime.Now,
+                Author = "SortingTest",
+                IsPublished = true,
+                IsCommentEnabled = false,
+                Rating = 2
+            });
+            postList.Add(new Post()
+            {
+                BlogId = Guid.Parse("11acc52b-040c-4456-8820-dd21f6122fbc"),
+                PostId = Guid.NewGuid(),
+                Title = "Highest Rated",
+                Description = "Description",
+                PostContent = "<p>Sample blog post</p>",
+                DateCreated = new DateTime(2018, 6, 13),
+                DateModified = DateTime.Now,
+                Author = "SortingTest",
+                IsPublished = true,
+                IsCommentEnabled = false,
+                Rating = 10
+            });
+            //not published, shouldnt sort
+            postList.Add(new Post()
+            {
+                BlogId = Guid.Parse("21acc52b-040c-4456-8820-dd21f6122fbc"),
+                PostId = Guid.NewGuid(),
+                Title = "No sort",
+                Description = "Description",
+                PostContent = "<p>Sample blog post</p>",
+                DateCreated = new DateTime(2019, 3, 27),
+                DateModified = DateTime.Now,
+                Author = "SortingTest",
+                IsPublished = false,
+                IsCommentEnabled = false,
+                Rating = 1
+            });
 
-            var controller = new HomeController(repo);
-            var result = controller.Index() as ViewResult;
-            Assert.AreEqual("Index", result.ViewName);
-
+            return postList;
         }
 
         [TestMethod]
         public void TestIndexMostRecentViewData()
         {
-            var controller = new HomeController(repo);
-            var result = controller.mostRecentSort();
-            var resultView = controller.Index() as ViewResult;
-            var post = (Post)resultView.ViewData.Model;
-            Assert.AreEqual(new DateTime(2019, 3, 27), post.DateCreated);
+            // Arrange
+            var mockRepo = new Mock<IPostRepository>();
+            mockRepo.Setup(repo => repo.GetAll())
+                .Returns(GetPosts());
+            var controller = new HomeController(mockRepo.Object);
+
+            // Act
+            var result = controller.mostRecentSort() as ViewResult;
+
+            // Assert
+            var post = (List<Post>)result.ViewData.Model;
+            Assert.AreEqual("Most Recent", post[0].Title);
+            Assert.AreEqual(new DateTime(2019, 3, 27), post[0].DateCreated);
+          
         }
 
         [TestMethod]
         public void TestIndexLeastRecentViewData()
         {
-            var controller = new HomeController(repo);
-            var result = controller.leastRecentSort();
-            var resultView = controller.Index() as ViewResult;
-            var post = (Post)resultView.ViewData.Model;
-            Assert.AreEqual(new DateTime(2015, 5, 10), post.DateCreated);
+            // Arrange
+            var mockRepo = new Mock<IPostRepository>();
+            mockRepo.Setup(repo => repo.GetAll())
+                .Returns(GetPosts());
+            var controller = new HomeController(mockRepo.Object);
+
+            // Act
+            var result = controller.leastRecentSort() as ViewResult;
+
+            // Assert
+            var post = (List<Post>)result.ViewData.Model;
+            Assert.AreEqual("Least Recent", post[0].Title);
+            Assert.AreEqual(new DateTime(2015, 5, 10), post[0].DateCreated);
+            
         }
 
         [TestMethod]
         public void TestIndexHighestRatedViewData()
         {
-            var controller = new HomeController(repo);
-            var result = controller.highestRatedSort();
-            var resultView = controller.Index() as ViewResult;
-            var post = (Post)resultView.ViewData.Model;
-            Assert.AreEqual(10, post.Rating);
+            // Arrange
+            var mockRepo = new Mock<IPostRepository>();
+            mockRepo.Setup(repo => repo.GetAll())
+                .Returns(GetPosts());
+            var controller = new HomeController(mockRepo.Object);
+
+            // Act
+            var result = controller.highestRatedSort() as ViewResult;
+
+            // Assert
+            var post = (List<Post>)result.ViewData.Model;
+            Assert.AreEqual("Highest Rated", post[0].Title);
+            Assert.AreEqual(10, post[0].Rating);
         }
     }
 }
+
