a0eb95940e5d1538c08cb934ca912e046844598d
Cole <cmulli@iastate.edu>
2020-04-26 18:36:39 -0500

    Added comments for posts

--- OpenSourceBlog/OpenSourceBlog/Database/Models/PostComment.cs
+++ OpenSourceBlog/OpenSourceBlog/Database/Models/PostComment.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace OpenSourceBlog.Database.Models
 {
+    [Table("PostComment")]
     public class PostComment
     {
         [Key]
